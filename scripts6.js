53a8ff7ed862087c074d555c91ab1d888adf13f4
yana <yamikmis13@gmail.com>
2022-07-19 17:49:14 +0300

    done

---
+++ scripts6.js
@@ -0,0 +1,5 @@
+let a = 5,
+    b = 7,
+    h = 10,
+    STrap = ((a + b) / 2) * h;
+console.log(STrap)
---
+++ scripts7.js
@@ -0,0 +1,5 @@
+let S = 2000000,
+    p = 10,
+    years = 5,
+    Pereplata = ((S * p) / 100) * years;
+console.log(Pereplata)
