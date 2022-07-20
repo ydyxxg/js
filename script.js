dc88e3a6c6558f8ae6a39324b9f731a0a9baa5fe
yana <yamikmis13@gmail.com>
2022-07-20 18:19:12 +0300

    done

---
+++ script.js
@@ -0,0 +1,7 @@
+let name = prompt('Введите своё имя'),
+    age = prompt('Введите своё имя'),
+    city = prompt('Введите своё имя'),
+    phone = prompt('Введите своё имя'),
+    email = prompt('Введите своё имя'),
+    company = prompt('Введите своё имя');
+console.log('Меня зовут ', name, '.Мне ', age,'лет. Я проживаю в городе', city,'и работаю в компании', company, '.Мои контактные данные:', phone, email);
