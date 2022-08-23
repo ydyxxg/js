d37c0861d5750c931c52ddc87681521868b75a1d
yana <yamikmis13@gmail.com>
2022-08-23 16:01:17 +0300

    tooltip not done

---
+++ script.js
@@ -0,0 +1,18 @@
+// const tooltip = () => {
+//     let elems = document.querySelectorAll('.tooltip');
+
+//     [...elems].forEach(item => {
+//         item.addEventListener('mouseenter', e => {
+//             createtooltip(e);
+//         });
+//     });
+
+//     function createtooltip(e){
+//         let elem = document.createElement('div');
+//         elem.classList.add('tooltip-element');
+//         elem.innerText = e.target.title;
+
+//         document.body.append(elem);
+//     }
+// }
+// tooltip();
